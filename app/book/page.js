07a88fb7cbf61cbba72d316309be3176da3dab52
OB1LAB <ob1chaam@gmail.com>
2024-05-08 02:39:20 +0300

    alpha

--- app/book/page.js
+++ app/book/page.js
@@ -9,8 +9,9 @@ import Image from "next/image";
 import { NavBarBook } from "@/modules/NavBar";
 
 const maxPage = 5;
+const mobileWidth = 660;
 
-function coefficientHeight() {
+function getSize() {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const [size, setSize] = useState([0, 0]);
   // eslint-disable-next-line react-hooks/rules-of-hooks
@@ -22,14 +23,14 @@ function coefficientHeight() {
     updateSize();
     return () => window.removeEventListener("resize", updateSize);
   }, []);
-  return size[1] / 1000;
+  return size;
 }
 
 export default function Book() {
   const bookRef = useRef();
   const [page, setPage] = useState(0);
   const [pages, setPages] = useState([]);
-  const coefHeight = coefficientHeight();
+  const [width, height] = getSize();
   useEffect(() => {
     setPages([`/book/preview.png`]);
     for (let i = 1; i <= maxPage; i++) {
@@ -50,39 +51,71 @@ export default function Book() {
       bookRef.current.children[page - 1].style.transform = `rotateY(0deg)`;
     }
   };
+  const getCoef = (elementSize) => {
+    if (width < 500) {
+      return (430 / 1920) * elementSize;
+    }
+    if (width < 1100) {
+      return (1100 / 1920) * elementSize;
+    }
+    if (width + height === 0) {
+      return 0.5 * elementSize;
+    }
+    return Math.min(width / 1920, height / 1000) * elementSize;
+  };
   return (
     <div className="bookPage">
       <NavBarBook />
-      <div className="bookElement">
-        <button className="nextButton" onClick={decrementPage}>
-          <Image src={prevIcon} alt="back" />
-        </button>
+      <div
+        className={width < mobileWidth ? `bookElement mobile` : `bookElement`}
+      >
+        {!(width < mobileWidth) && (
+          <button className="nextButton" onClick={decrementPage}>
+            <Image src={prevIcon} height={getCoef(112)} alt="back" />
+          </button>
+        )}
         <div className="book">
           <div className="lines">
-            <Image
-              height={748 * coefHeight}
-              src={yellowLine}
-              alt="yellowLine"
-            />
-            <Image height={720 * coefHeight} src={redLine} alt="redLine" />
-            <Image height={703 * coefHeight} src={blueLine} alt="blueLine" />
+            <Image height={getCoef(748)} src={yellowLine} alt="yellowLine" />
+            <Image height={getCoef(720)} src={redLine} alt="redLine" />
+            <Image height={getCoef(703)} src={blueLine} alt="blueLine" />
           </div>
-          <div className="bookImage" ref={bookRef}>
+          <div
+            className="bookImage"
+            ref={bookRef}
+            style={{
+              width: `${getCoef(606)}px`,
+              height: `${getCoef(620)}px`,
+              top: `${(getCoef(748) - getCoef(590)) / 2}px`,
+              left: `${width < mobileWidth ? `${(width - getCoef(606)) / 2}px` : (getCoef(250.5) + getCoef(278) + getCoef(250.5) + 60 - getCoef(606)) / 2}px`,
+            }}
+          >
             {pages.map((pageBook, pageIndex) => (
               <Image
                 style={{ zIndex: maxPage - pageIndex }}
                 src={pageBook}
-                width={550}
-                height={570}
+                width={getCoef(606)}
+                height={getCoef(620)}
                 alt={pageBook}
                 key={pageIndex}
               />
             ))}
           </div>
         </div>
-        <button className="backButton" onClick={incrementPage}>
-          <Image src={prevIcon} alt="next" />
-        </button>
+        {width < mobileWidth ? (
+          <div className="mobileButtons">
+            <button className="nextButton" onClick={decrementPage}>
+              <Image src={prevIcon} height={getCoef(112)} alt="back" />
+            </button>
+            <button className="backButton" onClick={incrementPage}>
+              <Image src={prevIcon} height={getCoef(112)} alt="next" />
+            </button>
+          </div>
+        ) : (
+          <button className="backButton" onClick={incrementPage}>
+            <Image src={prevIcon} height={getCoef(112)} alt="next" />
+          </button>
+        )}
       </div>
     </div>
   );
